6ebb79bef189639cc0b5a5b6ad809452e580fc13
Marek <marek.counts@outlook.com>
2018-05-15 10:23:05 -0400

    just minor updates to enable sending imcp message

--- Pust/src/bin/main.rs
+++ Pust/src/bin/main.rs
@@ -86,53 +86,6 @@ fn handle_ping_request(arg:&String) {
 
 }
 
-fn send_icmp_packet(interface: NetworkInterface, source_ip: Ipv4Addr, source_mac: MacAddr, target_ip: Ipv4Addr, target_mac: MacAddr) {
-       
-    let(mut tx, rx) = match pnet_datalink::channel(&interface, Default::default()) {
-        Ok(Channel::Ethernet(tx, rx)) => (tx, rx),
-        Ok(_) => panic!("Unknown channel type"),
-        Err(e) => panic!("Error happened {}", e),
-    };
-
-    let mut buff = [0,0,0,0,0,0,0,0];
-    let mut packet = MutableIcmpPacket::new( &mut buff);
-
-    
-    
-    //let(mut tx, rx) = match pnet_datalink::channel(network_interface, configuration);
-
-    /*
-    let(mut tx, _) = match pnet_datalink::channel(&interface, Default::default()) {
-        Ok(Channel::Ethernet(tx, rx)) => (tx, rx),
-        Ok(_) => panic!("Unknown channel type"),
-        Err(e) => panic!("Error happened {}", e),
-    };
-
-    let mut ethernet_buffer = [0u8; 42];
-    let mut ethernet_packet = MutableEthernetPacket::new(&mut ethernet_buffer).unwrap();
-
-    ethernet_packet.set_destination(target_mac);
-    ethernet_packet.set_source(source_mac);
-    ethernet_packet.set_ethertype(EtherTypes::Arp);
-
-    let mut arp_buffer = [0u8; 28];
-    let mut arp_packet = MutableArpPacket::new(&mut arp_buffer).unwrap();
-
-    arp_packet.set_hardware_type(ArpHardwareTypes::Ethernet);
-    arp_packet.set_protocol_type(EtherTypes::Ipv4);
-    arp_packet.set_hw_addr_len(6);
-    arp_packet.set_proto_addr_len(4);
-    arp_packet.set_operation(arp_operation);
-    arp_packet.set_sender_hw_addr(source_mac);
-    arp_packet.set_sender_proto_addr(source_ip);
-    arp_packet.set_target_hw_addr(target_mac);
-    arp_packet.set_target_proto_addr(target_ip);
-
-    ethernet_packet.set_payload(arp_packet.packet_mut());
-
-    tx.send_to(ethernet_packet.packet(), Some(interface));
-    */
-}
 /*
 fn main() {
     let iface_name = match env::args().nth(1) {
--- Pust/src/interface/mod.rs
+++ Pust/src/interface/mod.rs
@@ -1,6 +1,29 @@
 extern crate pnet;
 extern crate rand;
 extern crate pnet_datalink;
+extern crate pustlib;
+
+use pnet::datalink::{self};
+use std::net::Ipv4Addr;
+use pnet::packet::Packet;
+use pnet::packet::arp::ArpPacket;
+use pnet::packet::ethernet::{EtherTypes, EthernetPacket, MutableEthernetPacket};
+use pnet::packet::icmpv6::Icmpv6Packet;
+use pnet::packet::icmp::{echo_reply, echo_request, IcmpPacket, IcmpTypes, MutableIcmpPacket};
+use pnet::packet::ip::{IpNextHeaderProtocol, IpNextHeaderProtocols};
+use pnet::packet::ipv4::Ipv4Packet;
+use pnet::packet::ipv6::Ipv6Packet;
+use pnet::packet::tcp::TcpPacket;
+use pnet::packet::udp::UdpPacket;
+
+use pnet::datalink::Channel::Ethernet;
+
+use pnet_datalink::{Channel, NetworkInterface, MacAddr, ParseMacAddrErr};
+
+use std::env;
+use std::io::{self, Write};
+use std::process;
+use std::net::IpAddr;
 
 use pnet::datalink::{self};
 
@@ -24,11 +47,63 @@ pub fn get_active_interface() -> Option<pnet_datalink::NetworkInterface> {
         let thing = inter.mac_address();
         let thing2 = inter.is_up();
         let thing3 = inter.ips.first();
+        
         if let Some(thing3) = thing3 {
             println!("{}",thing3);
         }
-        active_interface = Some(inter.clone());
+        if (!inter.is_loopback()) {
+            active_interface = Some(inter.clone());
+        }
+        
     }
 
     return active_interface; 
+}
+
+
+
+fn send_icmp_packet(interface: NetworkInterface, source_ip: Ipv4Addr, source_mac: MacAddr, target_ip: Ipv4Addr, target_mac: MacAddr) {
+       
+    let(mut tx, rx) = match pnet_datalink::channel(&interface, Default::default()) {
+        Ok(Channel::Ethernet(tx, rx)) => (tx, rx),
+        Ok(_) => panic!("Unknown channel type"),
+        Err(e) => panic!("Error happened {}", e),
+    };
+
+    let mut buff = [0,0,0,0,0,0,0,0];
+    let mut packet = MutableIcmpPacket::new( &mut buff);
+
+    
+    
+    let(mut tx, rx) = match pnet_datalink::channel(network_interface, configuration);
+
+    let(mut tx, _) = match pnet_datalink::channel(&interface, Default::default()) {
+        Ok(Channel::Ethernet(tx, rx)) => (tx, rx),
+        Ok(_) => panic!("Unknown channel type"),
+        Err(e) => panic!("Error happened {}", e),
+    };
+
+    let mut ethernet_buffer = [0u8; 42];
+    let mut ethernet_packet = MutableEthernetPacket::new(&mut ethernet_buffer).unwrap();
+
+    ethernet_packet.set_destination(target_mac);
+    ethernet_packet.set_source(source_mac);
+    ethernet_packet.set_ethertype(EtherTypes::Arp);
+
+    let mut arp_buffer = [0u8; 28];
+    let mut arp_packet = MutableArpPacket::new(&mut arp_buffer).unwrap();
+
+    arp_packet.set_hardware_type(ArpHardwareTypes::Ethernet);
+    arp_packet.set_protocol_type(EtherTypes::Ipv4);
+    arp_packet.set_hw_addr_len(6);
+    arp_packet.set_proto_addr_len(4);
+    arp_packet.set_operation(arp_operation);
+    arp_packet.set_sender_hw_addr(source_mac);
+    arp_packet.set_sender_proto_addr(source_ip);
+    arp_packet.set_target_hw_addr(target_mac);
+    arp_packet.set_target_proto_addr(target_ip);
+
+    ethernet_packet.set_payload(arp_packet.packet_mut());
+
+    tx.send_to(ethernet_packet.packet(), Some(interface));
 }
